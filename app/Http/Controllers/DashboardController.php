f632a8cf1e4020dc798ceed8d687231f86e49d2b
Rosa Eliviani <38325869+rosa10@users.noreply.github.com>
2021-12-28 16:40:58 +0800

    Add files via upload

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,16 +6,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Obatalkes_m;
 use App\Models\Signa_m;
+use App\Models\Resep;
 
 class DashboardController extends Controller
 {
     public function index(){
         $obatalkes_m = Obatalkes_m::all();
         $signa_m = Signa_m::all();
+        $obat = Obatalkes_m::all();
+        $signa = Signa_m::all();
         return view('dashboard',[
-            'title'=>'Dasboard','obatalkes_m'=>$obatalkes_m, 'signa_m'=>$signa_m
+            'title'=>'Dasboard','obatalkes_m'=>$obatalkes_m, 'signa_m'=>$signa_m,'obat'=>$obat,'obat'=>$obat,'signa'=>$signa
         ]);
 
     }
-  
+  public function store(Request $request)
+    {
+        // dd($request);
+        Resep::create($request->all());
+        return redirect('/dashboard')-> with('status','Indikator Berhasil Ditambah');
+    }
 }
